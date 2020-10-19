6c40a5963fbf62925ad64172b1f4b8ca5f6264e0
andre-mit <andre.augusto222@gmail.com>
2020-10-19 18:04:20 -0300

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Head from 'next/head'
+
+import AzureLogo from '../assets/azure.svg'
+
+import { Container } from '../styles/pages/Home'
+
+const Home: React.FC = () => {
+    return (
+        <Container>
+            <Head>
+                <title>Template</title>
+            </Head>
+
+            <AzureLogo />
+            <h1>Base Next.js</h1>
+            <p>App with ReactJS + Next.js</p>
+        </Container>
+    )
+}
+export default Home
